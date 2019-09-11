a7b02e0401b5a01da22f1b03c197bbe6ebeb5910
Brian Gardiner <bgardiner9001@gmail.com>
2019-09-10 23:55:02 -0600

    navbar work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,14 @@ import React, {Component} from 'react'
 export default class Navbar extends Component {
     render(){
         return(
-            <h2 className="navbar">Navbar</h2>
+            <div className="navbar">
+                <h2 id="nav-logo">Nav Logo</h2>
+                <h3 className="nav-item">Home</h3>
+                <h3 className="nav-item">About</h3>
+                <h3 className="nav-item">Services</h3>
+                <h3 className="nav-item" id="nav-last-item">Contact</h3>
+            </div>
+            
         )
     }
 }
