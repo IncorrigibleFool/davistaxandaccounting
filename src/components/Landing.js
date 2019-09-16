7a855c34245dc1003e173dc2a257842e01494740
Brian Gardiner <bgardiner9001@gmail.com>
2019-09-15 23:58:01 -0600

    working on slideshow

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from './Navbar'
 import '../styles.css'
 
+
 export default function(){
     return(
         <div className="landing">
             <Navbar/>
+            <img src={require("../assets/business.jpg")} style={{"width": "100%", "height": "100%"}}/>
         </div>
     )
 }
