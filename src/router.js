31ef33fb5de65c5b2e1c7bc013dc888784c59d00
Brian Gardiner <bgardiner9001@gmail.com>
2019-09-17 19:28:59 -0600

    routing and components

--- src/router.js
+++ src/router.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Landing from './components/Landing'
+import About from './components/About'
+import Contact from './components/Contact'
 
 export default (
     <Switch>
         <Route exact path='/'>
             <Landing/>
+        </Route>
+        <Route path='/about'>
+            <About/>
+        </Route>
+        <Route path='/contact'>
+            <Contact/>
         </Route>
     </Switch>
 )
