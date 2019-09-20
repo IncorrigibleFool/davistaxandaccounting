e329281fe53ed6bbaa7732598d1f6a8f202d0df6
Brian Gardiner <bgardiner9001@gmail.com>
2019-09-19 23:57:41 -0600

    copyright and carousel work

---
+++ src/components/Copyright.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function(){
+    return(
+        <footer>
+            Copyright {'\u00a9'} 2019; Design by
+            <nbsp/>
+            <a href="http://www.briangardiner.info">Brian Gardiner</a>
+        </footer>
+    )
+}
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Copyright from './Copyright'
 import '../styles.css'
 
 export default function(){
@@ -7,18 +8,12 @@ export default function(){
         <div>
             <Navbar/>
             <div className="slideshow-container">
-                <div className="main-slideshow">
-                    {/* <img className="slideshow-pic-1" src={require("../assets/business.jpg")} style={{"width": "50%", "height": "50%"}}/> */}
+                <div className="main-slideshow" style={{"display": "flex", "flexDirection": "column", "justifyContent": "center", "alignItems": "center", "color": "white"}}>
+                    <h1>Certified Public Accountants<br/>for you and your business</h1>
+                    <p>We make life easier for business owners and individuals by providing high-quality tax and accounting services. Our professionals take care of your tax and accounting needs so that you can get on with life and focus on what is most important to you.</p>
                 </div>
             </div>
-            <div>Our Mission</div>
-            <p></p>
-            <div>Advisors you can trust</div>
-            <p></p>
-            <div>Get Help Today</div>
-            <p></p>
+            <Copyright/>
         </div>
-        
-
     )
 }
