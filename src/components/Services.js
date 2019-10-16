452076019586c1a3af563744bc923bf856218776
Brian Gardiner <bgardiner9001@gmail.com>
2019-10-15 23:33:22 -0600

    services text and placeholders

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function Services(){
     return(
-        <div id='services'>Services Test</div>
+        <div id='services'>
+            <h2>
+                Placeholder
+            </h2>
+            <div>
+                Business and Individual Income Tax
+                <p>Preparing your own tax return can be daunting and even frustrating. Without expert tax help, you could miss out on potential tax credits and deductions, resulting in higher tax liabilities. In addition, inaccurate tax returns could result in penalties and interest.</p>
+                <p>You need a trusted adviser who is an expert in the tax field to help you navigate through the complex and constantly changing tax laws. Tax season doesn’t need to be stressful. We will help you stay compliant and minimize your tax liabilities each tax season. We keep up on tax laws so that you don’t have to. We work hard to ensure that your federal and state income tax returns are prepared with a high level of accuracy and in a timely manner.</p>
+            </div>
+            <div>
+                Tax Representation
+                <p>Tax audits and even IRS correspondences can be perplexing and even frightening if you don’t know how to deal with the IRS. When not handled timely and appropriately, these audits and correspondences could get you in trouble with the IRS. You don’t need to face the IRS alone. We will represent you, provide information and explanation on your behalf, and enter into agreements with the IRS to ensure the best outcome for you.</p>
+            </div>
+            <div>
+                Tax Planning
+                <p>Do you have questions about how certain financial decisions will benefit or hurt you when tax filing season comes? Are you wondering how the new Tax Cuts and Job Acts will affect you and/or your business tax wise? If so, you need an experienced tax adviser to help you plan your financial affairs so as to minimize your personal and business tax liabilities. Our goal is to help you understand the tax benefits and consequences of your financial decisions so that you and your business can be the most tax-efficient.</p>
+            </div>
+            <div>
+                Bookkeeping Assistance
+                <p>Bookkeeping is an important but time-consuming business procedure that requires great attention to detail. All business owners need high-quality financial information in order to make good business decisions. However, bookkeeping softwares often have steep learning curves and require enormous time dedication, making it difficult for some business owners to keep up with the task. We can assist your business’ various bookkeeping needs, from performing monthly bank reconciliation to conducting high-level review of your books so that they are tax-ready. </p>
+            </div>
+            <div>
+                Quickbooks Setup and Training
+                <p>QuickBooks is a great and widely-used bookkeeping software for small businesses. However, setting up Quickbooks for small businesses can be overwhelming, especially for those with little or no accounting and bookkeeping background. Getting off to a good start is critical to your business getting the full benefit of the software and running its books smoothly going forward. We can help get your business up and running in Quickbooks in no time. </p>
+            </div>
+        </div>
     )
 }
