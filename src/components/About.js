b7d7ea5ee80df67485eec82c31f7343f43257072
Brian Gardiner <bgardiner9001@gmail.com>
2019-12-13 22:53:58 -0700

    css about

--- src/components/About.js
+++ src/components/About.js
@@ -10,13 +10,27 @@ export default class About extends Component{
         return(
             <div>
                 <Navbar/>
-                <div className="about-hero">About Test</div>
+                <h1 className="about-hero">About Us</h1>
+                <div className="spacing"></div>
                 <div id="profile-container">
-                    <div className="red-circle left-shadow">
-                        <img className="about-photo left" alt="trangImg" src={require("../assets/TRANG.jpg")}></img>
+                    <div className="individual-profile">
+                        <div className="red-circle left-shadow">
+                            <img className="about-photo left" alt="trangImg" src={require("../assets/TRANG.jpg")}></img>
+                        </div>
+                        <div className="profile-text">
+                            <div>Trang Davis</div>
+                            <div>CPA</div>   
+                        </div>
                     </div>
-                    <div className="red-circle right-shadow">
-                        <img className="about-photo right" alt="bradImg" src={require("../assets/BRAD.jpg")}></img>
+                    <div className="spacing"></div>
+                    <div className="individual-profile">
+                        <div className="profile-text">
+                            <div>Brad Davis</div>
+                            <div>CPA</div>
+                        </div>    
+                        <div className="red-circle right-shadow">
+                            <img className="about-photo right" alt="bradImg" src={require("../assets/BRAD.jpg")}></img>
+                        </div>
                     </div>
                 </div>  
             </div>
