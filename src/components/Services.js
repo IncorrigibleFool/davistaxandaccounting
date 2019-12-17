eebfe27adf601b25ee1fac4b581f4da7c93f9932
Brian Gardiner <bgardiner9001@gmail.com>
2019-12-16 23:43:42 -0700

    services styling prototypes

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,17 +3,25 @@ import React from 'react'
 export default function Services(){
     return(
         <div id='services'>
-            <h2>
-                Placeholder
-            </h2>
-            <div>
-                Business and Individual Income Tax
-                <p>Preparing your own tax return can be daunting and even frustrating. Without expert tax help, you could miss out on potential tax credits and deductions, resulting in higher tax liabilities. In addition, inaccurate tax returns could result in penalties and interest.</p>
-                <p>You need a trusted adviser who is an expert in the tax field to help you navigate through the complex and constantly changing tax laws. Tax season doesn’t need to be stressful. We will help you stay compliant and minimize your tax liabilities each tax season. We keep up on tax laws so that you don’t have to. We work hard to ensure that your federal and state income tax returns are prepared with a high level of accuracy and in a timely manner.</p>
+            <div className="spacing"></div>
+            <h1>
+                Services
+            </h1>
+            <div className="individual-service">
+                <div>image placeholder</div>
+                <div className="individual-service-text">
+                    <h2>Business and Individual Income Tax</h2>
+                    <p>Preparing your own tax return can be daunting and even frustrating. Without expert tax help, you could miss out on potential tax credits and deductions, resulting in higher tax liabilities. In addition, inaccurate tax returns could result in penalties and interest.</p>
+                    <p>You need a trusted adviser who is an expert in the tax field to help you navigate through the complex and constantly changing tax laws. Tax season doesn’t need to be stressful. We will help you stay compliant and minimize your tax liabilities each tax season. We keep up on tax laws so that you don’t have to. We work hard to ensure that your federal and state income tax returns are prepared with a high level of accuracy and in a timely manner.</p>
+                </div>
             </div>
-            <div>
-                Tax Representation
-                <p>Tax audits and even IRS correspondences can be perplexing and even frightening if you don’t know how to deal with the IRS. When not handled timely and appropriately, these audits and correspondences could get you in trouble with the IRS. You don’t need to face the IRS alone. We will represent you, provide information and explanation on your behalf, and enter into agreements with the IRS to ensure the best outcome for you.</p>
+            <div className="spacing"></div>
+            <div className="individual-service">
+                <div className="individual-service-text">
+                    <h2>Tax Representation</h2>
+                    <p>Tax audits and even IRS correspondences can be perplexing and even frightening if you don’t know how to deal with the IRS. When not handled timely and appropriately, these audits and correspondences could get you in trouble with the IRS. You don’t need to face the IRS alone. We will represent you, provide information and explanation on your behalf, and enter into agreements with the IRS to ensure the best outcome for you.</p>
+                </div>
+                <div>image placeholder</div>
             </div>
             <div>
                 Tax Planning
