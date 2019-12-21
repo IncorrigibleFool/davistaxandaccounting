943688f46a91ba01449ed687415c320ff0a151f9
Brian Gardiner <bgardiner9001@gmail.com>
2019-12-20 23:13:22 -0700

    service work and assets

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,10 +7,13 @@ export default function Services(){
         <div id='services'>
             <div className="spacing"></div>
             <h1>
-                Services
+                Our Services
             </h1>
+            <div className="spacing"></div>
             <div className="individual-service">
-                <div>image placeholder</div>
+                <div className="service-image-container">
+                    <img className="service-image" src={require("../assets/check.jpeg")} alt="checkmark"/>
+                </div>
                 <div className="individual-service-text">
                     <h2>Business and Individual Income Tax</h2>
                     <p>Preparing your own tax return can be daunting and even frustrating. Without expert tax help, you could miss out on potential tax credits and deductions, resulting in higher tax liabilities. In addition, inaccurate tax returns could result in penalties and interest.</p>
@@ -18,32 +21,48 @@ export default function Services(){
                 </div>
             </div>
             <div className="spacing"></div>
+            <div className="blue-divider"></div>
+            <div className="spacing"></div>
             <div className="individual-service">
+                <div className="service-image-container">
+                    <img className="service-image" src={require("../assets/scales.jpeg")} alt="scales"/>
+                </div>
                 <div className="individual-service-text">
                     <h2>Tax Representation</h2>
                     <p>Tax audits and even IRS correspondences can be perplexing and even frightening if you don’t know how to deal with the IRS. When not handled timely and appropriately, these audits and correspondences could get you in trouble with the IRS. You don’t need to face the IRS alone. We will represent you, provide information and explanation on your behalf, and enter into agreements with the IRS to ensure the best outcome for you.</p>
                 </div>
-                <div>image placeholder</div>
             </div>
             <div className="spacing"></div>
+            <div className="blue-divider"></div>
+            <div className="spacing"></div>
             <div className="individual-service">
-                <div>image placeholder</div>
+                <div className="service-image-container">
+                    <img className="service-image" src={require("../assets/compass.jpeg")} alt="compass"/>
+                </div>
                 <div className="individual-service-text">
                     <h2>Tax Planning</h2>
                     <p>Do you have questions about how certain financial decisions will benefit or hurt you when tax filing season comes? Are you wondering how the new Tax Cuts and Job Acts will affect you and/or your business tax wise? If so, you need an experienced tax adviser to help you plan your financial affairs so as to minimize your personal and business tax liabilities. Our goal is to help you understand the tax benefits and consequences of your financial decisions so that you and your business can be the most tax-efficient.</p>
                 </div>
             </div>
             <div className="spacing"></div>
+            <div className="blue-divider"></div>
+            <div className="spacing"></div>
             <div className="individual-service">
+                <div className="service-image-container">
+                    <img className="service-image" src={require("../assets/account.jpeg")} alt="accounts"/>
+                </div>
                 <div className="individual-service-text">
                     <h2>Bookkeeping Assistance</h2>
                     <p>Bookkeeping is an important but time-consuming business procedure that requires great attention to detail. All business owners need high-quality financial information in order to make good business decisions. However, bookkeeping softwares often have steep learning curves and require enormous time dedication, making it difficult for some business owners to keep up with the task. We can assist your business’ various bookkeeping needs, from performing monthly bank reconciliation to conducting high-level review of your books so that they are tax-ready. </p>
                 </div>
-                <div>image placeholder</div>
             </div>
             <div className="spacing"></div>
+            <div className="blue-divider"></div>
+            <div className="spacing"></div>
             <div className="individual-service">
-                <div>image placeholder</div>
+                <div className="service-image-container">
+                    <img className="service-image" src={require("../assets/computer.jpeg")} alt="computer"/>
+                </div>
                 <div className="individual-service-text">
                     <h2>Quickbooks Setup and Training</h2>
                     <p>QuickBooks is a great and widely-used bookkeeping software for small businesses. However, setting up Quickbooks for small businesses can be overwhelming, especially for those with little or no accounting and bookkeeping background. Getting off to a good start is critical to your business getting the full benefit of the software and running its books smoothly going forward. We can help get your business up and running in Quickbooks in no time. </p>
